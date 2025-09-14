abb2d055a0cff4ad30526146621decd90299abf0
Bryce Kerley <bkerley@brycekerley.net>
2025-09-14 13:56:39 -0400

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './src',
+  base: process.env.NODE_ENV === 'production' ? '/combo_foundation/' : '/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
